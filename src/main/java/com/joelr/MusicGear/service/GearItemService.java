42e7602bea607607cc6214f42c5e27ea5d088aae
Joel R <129562199+joelrennert@users.noreply.github.com>
2025-07-22 22:33:16 -0400

    Ignore .DS_Store file

--- src/main/java/com/joelr/MusicGear/service/GearItemService.java
+++ src/main/java/com/joelr/MusicGear/service/GearItemService.java
@@ -5,47 +5,48 @@ import com.joelr.MusicGear.model.GearItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class GearItemService {
 
+    private final GearItemDao gearItemDao;
+
     @Autowired
-    private GearItemDao gearItemDao;
+    public GearItemService(GearItemDao gearItemDao) {
+        this.gearItemDao = gearItemDao;
+    }
 
     // Get gear items by name or type
-    public List<GearItem> getGearItemsByNameOrType(String name, String type){
-        List<GearItem> gearItems = new ArrayList<>();
-        if(!type.equals("") && !name.equals("")){
+    public List<GearItem> getGearItemsByNameOrType(String name, String type) {
+        boolean hasName = name != null && !name.isBlank();
+        boolean hasType = type != null && !type.isBlank();
+
+        if (hasName && hasType) {
             return gearItemDao.getGearItemsByNameAndType("%" + name + "%", "%" + type + "%");
-        } else if(!type.equals("")){
+        } else if (hasType) {
             return gearItemDao.getGearItemsByType("%" + type + "%");
-        } else if(!name.equals("")){
+        } else if (hasName) {
             return gearItemDao.getGearItemsByName("%" + name + "%");
         }
+
         return gearItemDao.getGearItems();
     }
 
-    // Get gear item by id
     public GearItem getGearItemById(int id) {
         return gearItemDao.getGearItemById(id);
     }
 
-    // Add gear item
     public GearItem addGearItem(GearItem gearItem) {
         return gearItemDao.addGearItem(gearItem);
     }
 
-    // Update gear item
     public GearItem updateGearItem(int id, GearItem updatedGearItem) {
         updatedGearItem.setGearId(id);
         return gearItemDao.updateGearItem(updatedGearItem);
     }
 
-    // Remove gear item
     public void removeGearItem(int id) {
-            gearItemDao.removeGearItem(id);
+        gearItemDao.removeGearItem(id);
     }
-
 }
